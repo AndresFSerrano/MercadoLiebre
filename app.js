501b1bbfcce7413e66ae8d5d235510d236109484
Andrés Serrano <andresfserrano1@gmail.com>
2022-10-17 17:29:58 -0500

    Añadiendo animaciones, flex y detalles finales para subir a render

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require ('express') 
+const path = require ('path') 
+
 const app = express();
-const port = 3000
 
-app.listen(port, () =>{
-    console.log('Se está utilizando el puerto ' + port);
-});
-
-const publicPath = path.resolve(__dirname, "./public");
-
+const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname , "/views/home.html")
-    )})
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+
+app.listen (3030, () => console.log ("Server started: http://localhost:3030/") ); 
