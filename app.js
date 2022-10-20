9b2ab98b775798f7625df62e7711aebfbcb80887
Andrés Serrano <andresfserrano1@gmail.com>
2022-10-20 00:49:05 -0500

    Corriegiendo app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require ('express') 
-const path = require ('path') 
+const EXPRESS = require ('express') 
+const PATH = require ('path') 
 
-const app = express();
+const PORT = process.env.PORT || 3001;
 
-const publicPath = path.resolve(__dirname,'./public');
-const port = process.env.PORT || 3001;
-app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+const PUBLIC_PATH = PATH.resolve(__dirname,'./public');
+const APP = EXPRESS();
+
+APP.use(EXPRESS.static(PUBLIC_PATH));
+
+APP.get('/', (req, res) => {
+    res.sendFile(PATH.resolve(__dirname, './views/home.html'))
 });
 
-app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+APP.get("/register", (req, res) => {
+    res.sendFile(PATH.resolve(__dirname, "./views/register.html"))
 });
 
-app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+APP.get("/login", (req, res) => {
+    res.sendFile(PATH.resolve(__dirname, "./views/login.html"))
 });
 
 
-//app.listen (3030, () => console.log ("Server started: http://localhost:3030/") ); 
-
-app.listen(port, ()=>console.log('Servidor corriendo en el puerto ' + port));
+APP.listen (PORT, () => console.log (`Servidor iniciado en el puerto:  ${PORT} `) ); 
